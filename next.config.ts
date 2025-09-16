3f754f15fb799095830ce01e66775cc013e68ac0
SkinnyPetee <sri321ramhegde@gmail.com>
2025-09-16 19:53:23 +0530

    added root file in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  outputFileTracingRoot: __dirname, // ensures tracing starts in this folder
 };
 
 export default nextConfig;
